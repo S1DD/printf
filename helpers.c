85cd3f89abf1a2891cd45e70cdfb1329c900dfe6
Thabiso Molefe <thabisomolefe515@gmail.com>
2024-07-31 13:54:35 -0700

    Updated helpers.c

--- helpers.c
+++ helpers.c
@@ -13,7 +13,6 @@ void flushBuffer(char buffer[], int *buffer_idx)
 		write(1, buffer, *buffer_idx);
 		*buffer_idx = 0;
 	}
-
 }
 
 /**
@@ -30,39 +29,16 @@ int handleFormat(const char **format, va_list args,
 {
 	int count = 0;
 
-	switch (**format)
+	if (**format == '%')
 	{
-		case 'c':
-		{
-
-			char c = (char) va_arg(args, int);
-
-			count += printChar(c, buffer, buffer_idx);
-			break;
-		}
-		case 'd':
-		case 'i':
-		{
-			int num = va_arg(args, int);
-
-			count += printInt(num, buffer, buffer_idx);
-			break;
-		}
-		default:
-
-		{
-			const char *error_msg = "Unkown format specifier";
-
-			while (*error_msg)
-			{
-				count += printChar(*error_msg++, buffer, buffer_idx);
-			}
-			count += printChar('%', buffer, buffer_idx);
-			count += printChar(**format, buffer, buffer_idx);
-			break;
-		}
-
-		}
+		count += processFormatSpecifier(format, args, buffer, buffer_idx);
+	}
+	else
+	{
+		printChar(**format, buffer, buffer_idx);
+		count += 1;
+		(*format)++;
+	}
 
 	return (count);
 }
@@ -85,32 +61,6 @@ int printChar(char c, char buffer[], int *buffer_idx)
 	(*buffer_idx)++;
 	return (1);
 }
-
-/**
-* printString - Adds a string to the buffer.
-* @str: The string to print.
-* @buffer: The buffer where the string will be stored.
-* @buffer_idx: Pointer to the current index in the buffer.
-*
-* Return: The number of characters added to the buffer.
-*/
-int printString(const char *str, char buffer[], int *buffer_idx)
-{
-	int count = 0;
-
-	if (!str)
-	{
-		str = "(null)";
-	}
-
-	while (*str)
-	{
-		count += printChar(*str++, buffer, buffer_idx);
-	}
-
-	return (count);
-}
-
 /**
 * printInt - Adds an integer to the buffer.
 * @num: The integer to print.
@@ -122,17 +72,17 @@ int printString(const char *str, char buffer[], int *buffer_idx)
 int printInt(int num, char buffer[], int *buffer_idx)
 {
 	char str[12];
-	int i = 0, is_negative = 0, count = 0;
+	int i = 0, isNegative = 0, count = 0;
 
 	if (num == 0)
 	{
-		count += printChar(‘0’, buffer, buffer_idx);
+		count += printChar('0', buffer, buffer_idx);
 		return (count);
 	}
 
 	if (num < 0)
 	{
-		Is_negative = 1;
+		isNegative = 1;
 		num = -num;
 	}
 
@@ -141,7 +91,7 @@ int printInt(int num, char buffer[], int *buffer_idx)
 		str[i++] = (num % 10) + '0';
 		num /= 10;
 	}
-	if (is_negative)
+	if (isNegative)
 	{
 		str[i++] = '-';
 	}
@@ -152,6 +102,4 @@ int printInt(int num, char buffer[], int *buffer_idx)
 	}
 
 	return (count);
-
-
 }
